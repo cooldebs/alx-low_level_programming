53e7a4d3b6f17aa319a1b00260ef94df9a1a09ff
cooldebs <doduronbi83@gmail.com>
2022-09-13 15:54:20 -0700

    Write a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _abs: Function to print the absolute value of an integer.
+ * _abs- Function that prints the absolute value of an integer.
  * @n: Integer input.
  * Return: Absolue value of n.
  */
 int _abs(int n)
 {
-	int n;
-
 	n = _abs(-1);
 	printf("%d\n", n);
 	n = _abs(0);
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-My task 6
+There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you
