52c36724a7fc12dac8a321a52fdd6f8b92de56a9
cooldebs <doduronbi83@gmail.com>
2022-10-01 23:29:17 -0700

    strcat

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - a program that prints the minimum number of coins.
+ * main - a program that prints the minimum number of coins
  * @argc: argument count
  * @argv: argument value
  *
- * Return: 0
+ * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num, result = 0, i;
+	int x, total = 0, y, z;
+	int coins[5] = {25, 10, 5, 2, 1};
 
-	while (argc-- > 1)
+	if (argc != 2)
 	{
-		for (i = 0; argv[argc][i]; i++)
-		{
-			if (!(isdigit(argv[argc][i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		num = atoi(argv[argc]);
-		result += num;
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", result);
+	x = atoi(argv[1]);
+	if (x <= 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			z = x / c[i];
+			x -= z * c[i];
+			total += z;
+			if (x == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
 	return (0);
 }
+
