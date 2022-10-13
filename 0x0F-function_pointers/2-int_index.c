ee1f7d3ed6e02741a4f656aae0c840c19e72fa0d
cooldebs <doduronbi83@gmail.com>
2022-10-12 17:14:40 -0700

    Write a function that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,6 +4,7 @@
  * int_index - a function that searches for an integer.
  * @size: number of elements in the array.
  * @cmp: pointer to the function to be used to compare values
+ *
  * Return: returns the index of the first
  * element for which the cmp function does not return 0
  * If no element matches, return -1
