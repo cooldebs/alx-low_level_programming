e10e700b408fb5f7aa2efc6317c78b8a15b69640
cooldebs <doduronbi83@gmail.com>
2022-10-14 14:56:25 -0700

    Write a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			printf("nil");
+			printf("(nil)");
 		if (separator == NULL)
 			continue;
 		if (i < n - 1)
