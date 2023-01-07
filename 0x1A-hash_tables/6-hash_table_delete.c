1984e481ca5f0cf8dfe2d9ac57983e4764dce7fb
cooldebs <doduronbi83@gmail.com>
2023-01-06 20:13:14 -0800

    fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -13,7 +13,7 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
-			dummy = ht->array[i]
+			dummy = ht->array[i];
 			while (dummy)
 			{
 				holder = dummy->next;
