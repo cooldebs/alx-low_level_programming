0334ad75a93e0c78b4497f5c25d97efe7caf689f
cooldebs <doduronbi83@gmail.com>
2023-06-09 20:32:25 -0700

    Write a function that gives you the index of a key.

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,8 @@
  * @key: the key
  * @size: size of the array of the hash table
  *
- * Return: the index of the key/value
+ * Return:the index at which the key/value pair
+ * should be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
